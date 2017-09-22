41d2df95e117c748791555fed2548277fb003878
Fedorrychkov <fedorrychkov@yandex.ru>
2017-09-22 17:01:17 +0300

    #5 init input component

---
+++ internal/fedor/src/App/core/shared/webform/input/input.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './input.scss';
+
+export default class InputComponent extends Component {
+
+    render() {
+        return(
+            <div>
+            </div>
+        );
+    }
+}
