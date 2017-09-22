f22755687cc0a372f2a03839981e443856d508aa
Fedorrychkov <fedorrychkov@yandex.ru>
2017-09-22 17:47:35 +0300

    #6 textInput component

---
+++ internal/fedor/src/App/core/shared/webform/textInput/textInput.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './textInput.scss';
+
+export default class InputComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            
+        }
+    }
+
+    render() {
+        return(        
+            <div className="inputBox">
+                <label className="input_label" htmlFor={this.props.for}>{this.props.title} </label>
+                <input type={this.props.type} id={this.props.for} name={this.props.for} className="input" /> 
+            </div>
+
+        );
+    }
+}
--- internal/fedor/src/App/core/shared/webform/webform.js
+++ internal/fedor/src/App/core/shared/webform/webform.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 
 import './webform.scss';
 
+import InputComponent from './textInput/textInput';
+
 export default class WebformComponent extends Component {
 
     constructor(props) {
@@ -19,14 +21,7 @@ export default class WebformComponent extends Component {
                 <div className="form_title">
                     <h2>Hello, i'm <span className="job">Front<i>end</i> Developer</span></h2>
                 </div>
-                <div className="inputBox">
-                    <label className="input_label" htmlFor="name">Your Name</label>
-                    <input type="name" id="name" name="name" className="input" /> 
-                </div>
-                <div className="inputBox">
-                    <label className="input_label" htmlFor="name">Your Name</label>
-                    <input type="name" id="name" name="name" className="input" />
-                </div>
+                <InputComponent type="text" for="name" title="Your Name"/>
                 <div className="inputBox">
                     <select className="select">
                         <option>Freelance work</option>
